3cece8ca765c990a4a61ff05cca82785916a0d38
Michelle2104 <chidowemichelle@gmail.com>
2022-12-16 23:20:48 +0200

    .

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import full from "./img/react-icon-full.png"
+
+export default
+function Navbar() {
+    return (
+        <div className="container">
+            <img className="nav-icon" src={full} alt='React logo'/>
+            <h1 className="first-heading"> React Facts</h1>
+            <h1 className="sub-heading">React Project 1</h1>
+        </div>
+        
+    )
+}
